31f1f67d47108971f0957cc23b873e75dfdd8d23
smailik <smailik@pop-os.localdomain>
2022-12-03 00:24:20 +0700

    finish_paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(:id).paginate(page: params[:page], per_page: 7)
   end
 
   # GET /posts/1 or /posts/1.json
